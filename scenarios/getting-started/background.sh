6e970f9d635d33567cb572e94c697c57371f81e5
Jhan Silva <jhancarlossilva.c@gmail.com>
2023-05-30 21:46:43 -0600

    chore: Change code to delete k3s cluster first

--- scenarios/getting-started/background.sh
+++ scenarios/getting-started/background.sh
@@ -7,17 +7,7 @@ command -v rsync &> /dev/null || sudo apt-get install -y rsync
 command -v garden &> /dev/null || (curl -sSL https://github.com/garden-io/garden/releases/download/0.13.0/garden-0.13.0-linux-amd64.tar.gz | tar xz && \
 sudo mv linux-amd64/* /usr/local/bin)
 
-# Download k3s if it's not already installed
-if [ ! -f "./k3s" ]; then
-    wget -O k3s https://github.com/k3s-io/k3s/releases/download/v1.27.1%2Bk3s1/k3s
-    chmod +x k3s
-else
-    # Check if k3s is already running and stop it
-    if ps aux | grep '[k]3s server' > /dev/null; then
-        sudo kill $(ps aux | grep '[k]3s server' | awk '{print $2}')
-        /usr/local/bin/k3s-killall.sh # This literally kills the Kubernetes cluster if it's already there
-    fi
-fi
+/usr/local/bin/k3s-killall.sh # <- Because this lab uses k3s by default, this command is needed for me to create a new cluster with the options I need.
 
 # Start k3s server with host Docker iamge support and Traefik ingress controller disabled
 nohup sudo k3s server --docker --disable=traefik --write-kubeconfig-mode=644 --snapshotter native > /dev/null 2>&1 &
