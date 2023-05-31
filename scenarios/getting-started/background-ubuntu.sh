58a22f33d10c1907a16810d82de87b458bd1aa7a
Jhan Silva <jhancarlossilva.c@gmail.com>
2023-05-31 10:38:03 -0600

    create cluster

--- scenarios/getting-started/background-ubuntu.sh
+++ scenarios/getting-started/background-ubuntu.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 # Install rsync if it's not already installed
 command -v rsync &> /dev/null || sudo apt-get install -y rsync
 
-# # Install Garden if it's not already installed
+# Install Garden if it's not already installed
 command -v garden &> /dev/null || (curl -sSL https://github.com/garden-io/garden/releases/download/0.13.0/garden-0.13.0-linux-amd64.tar.gz | tar xz && \
 sudo mv linux-amd64/* /usr/local/bin)
 
@@ -23,15 +25,12 @@ else
 fi
 
 # Start k3s server with host Docker iamge support and Traefik ingress controller disabled
-nohup sudo k3s server --docker --disable=traefik --write-kubeconfig-mode=644 --snapshotter native > /dev/null 2>&1 &
+nohup sudo ./k3s server --docker --disable=traefik --write-kubeconfig-mode=644 --snapshotter native > /dev/null 2>&1 &
 
 # Copy k3s config to user's home directory
 mkdir -p ~/.kube
 sleep 5
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-
-git clone https://github.com/garden-io/quickstart-example.git && cd quickstart-example
-git config --global --add safe.directory '*'
 
 # Do not install NGINX ingress controller
 sed -i 's/\(providers:\)/\1\n  - name: local-kubernetes\n    environments: [local]\n    namespace: ${environment.namespace}\n    defaultHostname: ${var.base-hostname}\n    setupIngressController: null/' project.garden.yml
@@ -44,12 +43,5 @@ sed -i 's/hostname:/linkUrl:/' vote/garden.yml
 # Remove ingress blocks from result and api containers
 sed -i '/ingresses:/, /hostname: result.\${var.base-hostname}/d' api/garden.yml result/garden.yml
 
-# When you use --disable=traefik you need to wait for the traefik CRDs to be deleted before you can proceed.
-sleep 15 # Wait for Traefik jobs to be created
-kubectl wait --for=condition=complete --timeout=60s -n kube-system job/helm-delete-traefik-crd
-kubectl wait --for=condition=complete --timeout=60s -n kube-system job/helm-delete-traefik
-
-# This tells killercoda that the background is finished
+# Tell foreground script that background script is done
 echo done > /tmp/background0
-
-
